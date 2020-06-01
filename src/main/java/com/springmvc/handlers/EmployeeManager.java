64cf5c08603cd14c97c2b75b0b4c45f19358beea
Yann <shuliuyan@outlook.com>
2020-06-02 00:08:10 +0200

    Integrate Spring and SpringMVC IOC

--- src/main/java/com/springmvc/handlers/EmployeeManager.java
+++ src/main/java/com/springmvc/handlers/EmployeeManager.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,47 +23,33 @@ import com.springmvc.entities.Employee;
 @RequestMapping("/employeeManager")
 @Controller
 public class EmployeeManager {
+	@Autowired
+	EmployeeManagerDao employeeManagerDao;
+	@Autowired
+	DepartmentManagerDao departmentManagerDao;
 
-	EmployeeManagerDao employeeManagerDao = null;
-	DepartmentManagerDao departmentManagerDao = null;
-
-	public EmployeeManagerDao getEmployeeManagerDao(HttpServletRequest request) {
-		if (employeeManagerDao == null) {
-			ApplicationContext ac = (ApplicationContext) request.getServletContext().getAttribute("ApplicationContext");
-			employeeManagerDao = ac.getBean(EmployeeManagerDao.class);
-		}
-		return employeeManagerDao;
-	}
-
-	public DepartmentManagerDao getDepartmentManagerDao(HttpServletRequest request) {
-		if (departmentManagerDao == null) {
-			ApplicationContext ac = (ApplicationContext) request.getServletContext().getAttribute("ApplicationContext");
-			departmentManagerDao = ac.getBean(DepartmentManagerDao.class);
-		}
-		return departmentManagerDao;
-	}
-
+	
 	@RequestMapping(value = "/list")
-	public String getList(Map<String, Object> map, HttpServletRequest request) {
+	public String getList(Map<String, Object> map) {
 		List<Employee> lst = new ArrayList<>();
-		lst = getEmployeeManagerDao(request).getList();
+		lst = employeeManagerDao.getList();
 		map.put("employeelist", lst);
 		return "list";
 	}
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-	public String delete(@PathVariable Integer id, HttpServletRequest request) {
-		getEmployeeManagerDao(request).deleteEmployee(id);
+	public String delete(@PathVariable Integer id) {
+		employeeManagerDao.deleteEmployee(id);
 
 		return "redirect:/employeeManager/list";
 	}
 
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-	public String getEdit(@PathVariable Integer id, HttpServletRequest request, Map<String, Object> map) {
-		Employee empEdit = getEmployeeManagerDao(request).getEmployee(id);
+	public String getEdit(@PathVariable Integer id, Map<String, Object> map) {
+		Employee empEdit = employeeManagerDao.getEmployee(id);
 		map.put("employeeEdit", empEdit);
 		List<Department> lst = new ArrayList<>();
-		lst = getDepartmentManagerDao(request).getDepts();
+		lst = departmentManagerDao.getDepts();
 		map.put("deptlist", lst);
 		return "edit";
 	}
@@ -70,38 +58,37 @@ public class EmployeeManager {
 	@RequestMapping(value = "/input")
 	public String getDept(Map<String, Object> map, HttpServletRequest request) {
 		List<Department> lst = new ArrayList<>();
-		lst = getDepartmentManagerDao(request).getDepts();
+		lst = departmentManagerDao.getDepts();
 		map.put("deptlist", lst);
 		map.put("employee", new Employee());
 		return "input";
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String addEmployee(Map<String, Object> map, HttpServletRequest request, Employee employee) {
+	public String addEmployee(Map<String, Object> map, Employee employee) {
 
-		Department dept = getDepartmentManagerDao(request).getDept(employee.getDepartment().getId());
+		Department dept = departmentManagerDao.getDept(employee.getDepartment().getId());
 		employee.setDepartment(dept);
-		getEmployeeManagerDao(request).save(employee);
+		employeeManagerDao.save(employee);
 		return "redirect:/employeeManager/list";
 	}
 
 	@ModelAttribute
-	public void getModel(@RequestParam(value = "id", required = false) Integer id, Model model,
-			HttpServletRequest request) {
+	public void getModel(@RequestParam(value = "id", required = false) Integer id, Model model) {
 		if (id != null) {
-			Employee empEdit = getEmployeeManagerDao(request).getEmployee(id);
+			Employee empEdit =employeeManagerDao.getEmployee(id);
 			System.out.println("Gets model from database:" + empEdit);
 			model.addAttribute("employeeEdit", empEdit);
 		}
 	}
 
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String updateEmployee( Map<String, Object> map, HttpServletRequest request,
+	public String updateEmployee( Map<String, Object> map,
 			@ModelAttribute(name = "employeeEdit") Employee employee) {
 
-		Department dept = getDepartmentManagerDao(request).getDept(employee.getDepartment().getId());
+		Department dept = departmentManagerDao.getDept(employee.getDepartment().getId());
 		employee.setDepartment(dept);
-		getEmployeeManagerDao(request).update(employee);
+		employeeManagerDao.update(employee);
 		return "redirect:/employeeManager/list";
 	}
 }
